2b898f3d88de352248568794749cb0fc1ae6e2bc
chilliesdev <matthewkayode116@gmail.com>
2020-06-14 13:24:59 +0100

    fix production login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,36 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
 
 class AuthController extends Controller
 {
-	public function login(Request $request)
-	{
-		$http = new \GuzzleHttp\Client;
+	public $successStatus = 200;
 
-		try {
-			$reponse = $http->post(env('PASSPORT_LOGIN_ENDPOINT'),[
-				'form_params' => [
-					'grant_type' => 'password',
-					'client_id' => env('PASSPORT_CLIENT_ID'),
-					'client_secret' => env('PASSPORT_CLIENT_SECRET'),
-					'username' => $request->username,
-					'password' => $request->password,
-				]
-			]);
-
-			return $reponse->getBody();
-		} catch (\GuzzleHttp\Exception\BadResponseException $e){
-			if ($e->getCode() == 400){
-				return response()->json('400');
-				// return response()->json('Invalid request. Please enter a username or a password.');
-			} else if ($e->getCode() == 401){
-				// return response()->json('Your credentials are incorrect. Please try again');
-				return response()->json('401');
-			}
-
-			return response()->json($e);
-		}
+	public function login(){ 
+		if(Auth::attempt(['email' => request('username'), 'password' => request('password')])){ 
+				$user = Auth::user(); 
+				$success['access_token'] =  $user->createToken('MyApp')->accessToken; 
+				return response()->json($success, $this->successStatus); 
+		} 
+		else{ 
+				return response()->json(['error'=>'Unauthorised'], 401); 
+		} 
 	}
 
 	public function logout()
